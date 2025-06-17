cbfd7a6a44d1d7f2ae8255d9fe22efb8d17ff5f5
Leandro Jiménez <leandroj21p@gmail.com>
2025-06-17 18:49:53 -0400

    initial commit

---
+++ src/test/java/org/example/imsbackend/ImsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.imsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
