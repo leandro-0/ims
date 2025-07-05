d567073a396214c91bdee30f4d730422d8d3ffda
Leandro Jiménez <leandroj21p@gmail.com>
2025-07-05 16:55:49 -0400

    refactor: use DTO instead of Product class

--- backend/src/main/java/org/example/imsbackend/validators/ValidCategoryValidator.java
+++ backend/src/main/java/org/example/imsbackend/validators/ValidCategoryValidator.java
@@ -6,13 +6,6 @@ import org.example.imsbackend.enums.Category;
 import org.example.imsbackend.annotations.ValidCategory;
 
 public class ValidCategoryValidator implements ConstraintValidator<ValidCategory, Object> {
-    private boolean useDisplayName;
-
-    @Override
-    public void initialize(ValidCategory constraintAnnotation) {
-        this.useDisplayName = constraintAnnotation.useDisplayName();
-    }
-
     @Override
     public boolean isValid(Object value, ConstraintValidatorContext context) {
         if (value == null || value instanceof Category)
@@ -20,21 +13,11 @@ public class ValidCategoryValidator implements ConstraintValidator<ValidCategory
         if (!(value instanceof String))
             return false;
 
-        if (useDisplayName) {
-            for (Category category : Category.values()) {
-                if (category.getDisplayName().equals((String) value)) {
-                    return true;
-                }
-            }
-        } else {
-            try {
-                Category.valueOf(((String) value).toUpperCase());
-                return true;
-            } catch (IllegalArgumentException e) {
-                return false;
-            }
+        try {
+            Category.valueOf(((String) value).toUpperCase());
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
         }
-
-        return false;
     }
 }
