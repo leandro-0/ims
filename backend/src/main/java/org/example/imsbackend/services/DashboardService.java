c109c81b812051555783f01afee041c9b3bbbb6d
Josue <jdgg0001@ce.pucmm.edu.do>
2025-08-26 03:07:25 -0400

    fix: update stock movement statistics to reflect last 24 hours

--- backend/src/main/java/org/example/imsbackend/services/DashboardService.java
+++ backend/src/main/java/org/example/imsbackend/services/DashboardService.java
@@ -58,9 +58,9 @@ public class DashboardService {
 
     // Movements last 24 hours
     private StatsData.MovementsLast24Hours getStockMovementLast24Hours(){
-        long in = stockMovementService.countStockMovementsByTypeInLast24Hours(StockMovementType.INCOMING, LocalDateTime.now());
-        long out = stockMovementService.countStockMovementsByTypeInLast24Hours(StockMovementType.OUTGOING, LocalDateTime.now());
-        List<UsernameCount> usernameCounts = stockMovementService.countStockMovementsByUsernameInLast24Hours(LocalDateTime.now());
+        long in = stockMovementService.countStockMovementsByTypeInLast24Hours(StockMovementType.INCOMING, LocalDateTime.now().minusDays(1));
+        long out = stockMovementService.countStockMovementsByTypeInLast24Hours(StockMovementType.OUTGOING, LocalDateTime.now().minusDays(1));
+        List<UsernameCount> usernameCounts = stockMovementService.countStockMovementsByUsernameInLast24Hours(LocalDateTime.now().minusDays(1));
         //just top 3
         if (usernameCounts.size() > 3) {
             usernameCounts = usernameCounts.subList(0, 3);
