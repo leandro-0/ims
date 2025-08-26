52c86d2e544d421bc2b11d954a4b76de21d2eef9
Leandro Jiménez <leandroj21p@gmail.com>
2025-08-26 03:09:21 -0400

    feat: refresh token

--- frontend/src/core/auth-options.ts
+++ frontend/src/core/auth-options.ts
@@ -1,5 +1,6 @@
 import { NextAuthOptions } from "next-auth"
 import { jwtDecode } from "jwt-decode"
+import { JWT } from "next-auth/jwt"
 
 const clientId = process.env.KEYCLOAK_CLIENT_ID || "ims"
 const serverIssuer = process.env.KEYCLOAK_ISSUER_SERVER || process.env.KEYCLOAK_ISSUER || "http://localhost:7080/realms/ims-realm"
@@ -15,16 +16,39 @@ interface KeycloakProfile {
   }
 }
 
-async function validateToken(token: string): Promise<boolean> {
+async function refreshToken(token: JWT): Promise<JWT | null> {
   try {
-    const response = await fetch(`${serverIssuer}/protocol/openid-connect/userinfo`, {
+    const response = await fetch(`${serverIssuer}/protocol/openid-connect/token`, {
+      method: 'POST',
       headers: {
-        'Authorization': `Bearer ${token}`
-      }
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        grant_type: 'refresh_token',
+        client_id: clientId,
+        refresh_token: token.refreshToken || '',
+      }),
     })
-    return response.ok
-  } catch (error) {
-    return false
+
+    if (!response.ok) {
+      return null
+    }
+
+    const refreshedTokens = await response.json()
+    token.accessToken = refreshedTokens.access_token
+    token.refreshToken = refreshedTokens.refresh_token || token.refreshToken
+    token.expiresAt = Date.now() + (Number(refreshedTokens.expires_in) || 3600) * 1000
+
+    const decodedToken = jwtDecode(refreshedTokens?.access_token)
+    if (decodedToken && typeof decodedToken !== 'string') {
+      token.roles = decodedToken?.resource_access.ims.roles || []
+    }
+
+    return token
+  }
+  catch (error) {
+    console.error('Error refreshing access token:', error)
+    return null
   }
 }
 
@@ -82,7 +106,11 @@ export const authOptions: NextAuthOptions = {
       }
 
       if (token.accessToken && token.expiresAt && Date.now() > token.expiresAt) {
-        return {}
+        if (process.env.NODE_ENV === 'development') {
+          console.log('\n\nAccess token expired, refreshing...\n\n')
+        }
+
+        return await refreshToken(token)
       }
 
       return token
